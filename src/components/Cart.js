bc260bc2bd0801f893a6dc743dbd0afb800d895c
Victor Dallé <dallevictor@gmail.com>
2023-02-05 23:00:58 +0100

    Ajout des fichiers importants

---
+++ src/components/Cart.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "../styles/Cart.css";
+
+function Cart({ cart, updateCart }) {
+    const monsteraPrice = 8;
+    const [isOpen, setIsOpen] = useState(true);
+
+    return isOpen ? (
+        <div className="lmj-cart">
+            <button
+                className="lmj-cart-toggle-button"
+                onClick={() => setIsOpen(false)}
+            >
+                Fermer
+            </button>
+            <h2>Panier</h2>
+            <h3>Total : {monsteraPrice * cart}€</h3>
+            <button onClick={() => updateCart(0)}>Vider le panier</button>
+        </div>
+    ) : (
+        <div className="lmj-cart-closed">
+            <button
+                className="lmj-cart-toggle-button"
+                onClick={() => setIsOpen(true)}
+            >
+                Ouvrir le Panier
+            </button>
+        </div>
+    );
+}
+
+export default Cart;
---
+++ src/components/ShoppingList.js
@@ -0,0 +1,38 @@
+import { plantList } from "../datas/plantList";
+import PlantItem from "./PlantItem";
+import "../styles/ShoppingList.css";
+
+function ShoppingList({ cart, updateCart }) {
+    const categories = plantList.reduce(
+        (acc, plant) =>
+            acc.includes(plant.category) ? acc : acc.concat(plant.category),
+        []
+    );
+
+    return (
+        <div className="lmj-shopping-list">
+            <ul>
+                {categories.map((cat) => (
+                    <li key={cat}>{cat}</li>
+                ))}
+            </ul>
+            <ul className="lmj-plant-list">
+                {plantList.map(({ id, cover, name, water, light }) => (
+                    <div key={id}>
+                        <PlantItem
+                            cover={cover}
+                            name={name}
+                            water={water}
+                            light={light}
+                        />
+                        <button onClick={() => updateCart(cart + 1)}>
+                            Ajouter
+                        </button>
+                    </div>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default ShoppingList;
