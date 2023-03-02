883f20a488a4e9712a3fffb638b29bf130cd32de
Victor Dallé <dallevictor@gmail.com>
2023-03-02 17:18:15 +0100

    Ajouts feature

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,7 @@
-import '../styles/Banner.css'
+import "../styles/Banner.css";
 
 function Banner({ children }) {
-	return <div className='lmj-banner'>{children}</div>
+    return <div className="lmj-banner">{children}</div>;
 }
 
-export default Banner
+export default Banner;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,6 +1,7 @@
 import { plantList } from "../data/plantList";
 import PlantItem from "./PlantItem";
 import "../styles/ShoppingList.css";
+import { useEffect, useState } from "react";
 
 function ShoppingList({ cart, updateCart }) {
     const categories = plantList.reduce(
@@ -25,30 +26,69 @@ function ShoppingList({ cart, updateCart }) {
         }
     }
 
-    return (
-        <div className="lmj-shopping-list">
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    // Note: the empty deps array [] means
+    // this useEffect will run once
+    // similar to componentDidMount()
+    useEffect(() => {
+        fetch("https://swapi.dev/api/people/?format=json")
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result.results);
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            );
+    }, []);
+
+    // return (
+    //     <div className="lmj-shopping-list">
+    //         <ul>
+    //             {categories.map((cat) => (
+    //                 <li key={cat}>{cat}</li>
+    //             ))}
+    //         </ul>
+    //         <ul className="lmj-plant-list">
+    //             {plantList.map(({ id, cover, name, water, light, price }) => (
+    //                 <div key={id}>
+    //                     <PlantItem
+    //                         cover={cover}
+    //                         name={name}
+    //                         water={water}
+    //                         light={light}
+    //                     />
+    //                     <button onClick={() => addToCart(name, price)}>
+    //                         Ajouter
+    //                     </button>
+    //                 </div>
+    //             ))}
+    //         </ul>
+    //     </div>
+    // );
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
             <ul>
-                {categories.map((cat) => (
-                    <li key={cat}>{cat}</li>
+                {items.map((item, index) => (
+                    <li key={index}>{item.name}</li>
                 ))}
             </ul>
-            <ul className="lmj-plant-list">
-                {plantList.map(({ id, cover, name, water, light, price }) => (
-                    <div key={id}>
-                        <PlantItem
-                            cover={cover}
-                            name={name}
-                            water={water}
-                            light={light}
-                        />
-                        <button onClick={() => addToCart(name, price)}>
-                            Ajouter
-                        </button>
-                    </div>
-                ))}
-            </ul>
-        </div>
-    );
+        );
+    }
 }
 
 export default ShoppingList;
